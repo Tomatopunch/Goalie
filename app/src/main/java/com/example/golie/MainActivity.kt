b22073c512f56cee12258d0a06f62af4cf7e6d69
Dennis Andersson <dennisfram@hotmail.com>
2020-02-06 19:52:12 +0100

    idk

--- app/src/main/java/com/example/golie/MainActivity.kt
+++ app/src/main/java/com/example/golie/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.golie.ui.shop.ShopFragment
+import com.example.golie.ui.shop.reward.CreateReward
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShopFragment.Interface {
+
+    var newFragment = CreateReward()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +29,38 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+
+    override fun theButtonWasClicked() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.frameLayout) as CreateReward?
+
+        // We're in two-pane layout. This is for bigger screens.
+        if(fragment != null){ }
+
+        else {
+            // This is a one-pane layout. This is what we always use.
+            val args = Bundle()
+
+            // args.putInt(categoryFragment.ARG_POSITION, position)
+            newFragment.arguments = args
+
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.nav_host_fragment, newFragment)
+
+            //transaction.addToBackStack(null)
+
+            transaction.commit()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.remove(newFragment)
+        transaction.commit()
+
     }
 }
--- app/src/main/java/com/example/golie/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/example/golie/ui/shop/ShopFragment.kt
@@ -6,8 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
 
 import com.example.golie.R
+import com.example.golie.ToDo
+import com.example.golie.toDoRepository
+import kotlinx.android.synthetic.main.shop_fragment.*
+import kotlinx.android.synthetic.main.shop_fragment.view.*
+
+
+
 
 class ShopFragment : Fragment() {
 
@@ -16,18 +25,82 @@ class ShopFragment : Fragment() {
     }
 
     private lateinit var viewModel: ShopViewModel
+    private lateinit var adapter: ArrayAdapter<ToDo>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.shop_fragment, container, false)
+
+        val view = inflater!!.inflate(R.layout.shop_fragment, container, false)
+        val listView = view.shop_listView
+
+        adapter = ArrayAdapter(
+            context!!,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            toDoRepository.getAllToDos()
+        )
+
+        listView.adapter = adapter
+        listView.setOnItemClickListener{ _, _, _, _ ->
+
+            AlertDialog.Builder(context!!)
+                .setTitle("Buy")
+                .setMessage("Are you sure you want to buy this item?")
+                .setPositiveButton(
+                    "Yes"
+                ) { _, _->
+                    AlertDialog.Builder(context!!)
+                        .setTitle("That's great!")
+                        .setMessage("Your new balance: XXX")
+                        .setPositiveButton(
+                            "Enjoy your new reward!"
+                        ) { _, _->
+                        }.show()
+                }.setNegativeButton(
+                    "No"
+                ) { _, _->
+                }.show()
+        }
+        return view
     }
+
+    // Do something with your button!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val buttonCreateReward = shop_floatingActionButton
+
+        buttonCreateReward.setOnClickListener {
+            // Here we cast main activity to the interface (below) and this is possible because
+            // main activity extends this interface
+            var intf = context!! as Interface
+            intf.theButtonWasClicked()
+            //activity.theButtonWasClicked()
+        }
+    }
+
+    // No clue what this is yet
+    interface  Interface {
+        fun theButtonWasClicked()
+    }
+
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    //Notify changes when you come back
+    override fun onStart() {
+        super.onStart()
+        adapter.notifyDataSetChanged()
+    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ShopViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
