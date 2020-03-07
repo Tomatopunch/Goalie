64c0b6995db4ee20cab87d6030038fd7d8331a34
JosefinLindvall <lijo18zu@student.ju.se>
2020-03-07 22:05:07 +0100

    Did some work on log in but unsure how to continue, need to ask linus for help

--- app/src/main/java/com/example/golie/MainActivity.kt
+++ app/src/main/java/com/example/golie/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.golie
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,11 +10,18 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.golie.ui.category.addGoal.AddGoalFragment
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     var newFragment = AddGoalFragment()
 
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,61 +32,68 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-
-        /*val appBarConfiguration = AppBarConfiguration(
+        val appBarConfiguration = AppBarConfiguration( //Disables back button in these files
             setOf(
-                R.id.nav_favorite, R.id.nav_home, R.id.nav_shop
+                R.id.nav_home, R.id.nav_shop
             )
-        )*/
+        )
 
-        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        ////////////////////////////// LOG IN ////////////////////////////////////////////////////////////
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
     }
 
 
-    // Sparat för att Linus ska se skillnaden.
 
-    /*override fun theButtonWasClicked() {
-        Log.d("test", "händer inget")
-        /*val homeFragment = supportFragmentManager.findFragmentById(R.id.addGoalFragment) as AddGoalFragment?
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        if(homeFragment != null){
-            // We're in two-pane layout. This is for bigger screens.
-            Log.d("kaos", "Det blev feeeeeel")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
         }
-
-        else{*/
-            // This is a one-pane layout. This is what we always use.
-            val args = Bundle()
-            // args.putInt(categoryFragment.ARG_POSITION, position)
-            newFragment.arguments = args
-
-            val transaction = supportFragmentManager.beginTransaction()
-
-            transaction.add(R.id.nav_host_fragment, newFragment)
-                transaction.setReorderingAllowed(true)
-            transaction.addToBackStack(null)
-
-            transaction.commit()
-        //}
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        val transaction = supportFragmentManager.beginTransaction()
+    override fun onStart() {
+        super.onStart()
 
-        transaction.remove(newFragment)
-        transaction.commit()
-
-    }*/
-
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account: GoogleSignInAccount  = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+    }
 }
 
 
-
-//class MainActivity : AppCompatActivity(), categoryFragment.Interface{
-
-//}
